654ea828a1dae3c959211a85057a5b15275a5d85
Dex-kei <dexkei145@gmail.com>
2021-04-16 11:43:46 +0800

    老師範例

---
+++ proj60/test/a20210401-07-array-json.php
@@ -0,0 +1,26 @@
+<?php
+$persons = [
+    [
+        'name' => 'Bill',
+        'age' => 27,
+        'gender' => 'male',
+    ],
+    [
+        'name' => '大衛',
+        'age' => 25,
+        'gender' => 'male',
+    ],
+    [
+        'name' => 'Flora',
+        'age' => 22,
+        'gender' => 'female',
+    ],
+    [
+        'name' => 'Mary',
+        'age' => 28,
+        'gender' => 'female',
+    ]
+];
+
+// header('Content-Type: application/json');
+echo json_encode($persons, JSON_UNESCAPED_UNICODE);
---
+++ proj60/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5445980bf96262f33c1cca13129ada6f93ea62bb',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5445980bf96262f33c1cca13129ada6f93ea62bb',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.4.0',
+      'version' => '6.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '050d430203105c27c30efd1dce7aa421ad882d01',
+    ),
+  ),
+);
